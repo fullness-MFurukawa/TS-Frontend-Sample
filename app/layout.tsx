e3877761e1cc045c72ec3aebc8b1803a1fa5ceeb
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-03-29 17:11:31 +0900

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,16 @@
  * List3-2 Rootレイアウト
  */
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+
 // { children } を受け取り、型にReact.ReactNodeを指定する
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja">
+    <html lang="ja" className={cn("font-sans", geist.variable)}>
       <body>
         {/* 全ページ共通で表示されるヘッダー */}
         <header className="bg-blue-600 text-white p-4 font-bold">
